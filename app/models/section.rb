e72080414ed2ea9ea548896d8b1b167f40b723c6
Momen <personal.momen@gmail.com>
2026-06-29 19:04:37 -0600

    Student Management System Version 1.0.0

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,6 @@
 class Section < ApplicationRecord
   belongs_to :course
   has_and_belongs_to_many :students
-  validates :section_number, presence: true
+  validates :section_number, presence: true, uniqueness: { scope: :course_id, message: "has already been taken for this course" }
   validates :course_id, presence: true
 end
