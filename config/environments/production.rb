8caf30057b49a6bf36539c8d4bf761bd586234c7
Momen <personal.momen@gmail.com>
2026-08-03 20:50:40 -0600

    Add deployment files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  if ENV["RENDER"].present?
+    config.public_file_server.enabled = true
+  end
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
